b7960552cf26dc21edd1264bed36c3e8382bb3b9
Raevan Gregson <regregson@fullsail.edu>
2019-04-04 00:40:15 -0400

    grid created

--- src/app.jsx
+++ src/app.jsx
@@ -5,7 +5,7 @@ import './styles.css'
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className='mainContent'>
           <Grid/>
       </div>
     );
--- src/components/grid.jsx
+++ src/components/grid.jsx
@@ -1,27 +1,52 @@
 import React, { Component } from "react";
-import {randomizeRocks} from '../utils.js';
 
 class Grid extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            rocks:[],
-            roverStart:null,
+            grid: null,
+            rocks: [],
+            roverStart: null,
         }
+        this.createGrid = this.createGrid.bind(this)
+        this.randomizeRocks = this.randomizeRocks.bind(this)
     }
 
     componentWillMount() {
-        let rocks = randomizeRocks()
+        let rocks = this.randomizeRocks()
+        let grid = this.state.grid
+        if (grid == null) grid = this.createGrid()
         this.setState({
-            rocks
+            rocks,
+            grid
         })
     }
 
+    //this function randomizes rocks that act as an obstacle
+    randomizeRocks() {
+        let rocks = []
+        for (var x = 1; rocks.length < 5; x++) {
+            let randomTile = Math.floor(Math.random() * 36) + 1
+            if (!rocks.includes(randomTile)) {
+                rocks.push(randomTile)
+            }
+        }
+        return rocks
+    }
+
+    createGrid() {
+        let gridItems = []
+        for (var x = 1; x <= 36; x++) {
+            gridItems.push(<div key={x} className="grid-item"></div>)
+        }
+        return <div className='grid-container'>{gridItems}</div>
+    }
+
     render() {
-        console.log(this.state.rocks)
+        console.log(this.state.grid)
         return (
             <div>
-                hello
+               {this.state.grid}
             </div>
         );
     }
